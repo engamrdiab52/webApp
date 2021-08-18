81fc32b0c4d2576c063f8d5b3393422cadeb2f74
amr abdelhamid <engamrdiab52@gmail.com>
2021-08-18 05:19:02 +0200

    support offline mode

---
+++ app/src/main/java/com/afdal/webapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.afdal.webapp
+
+import android.annotation.SuppressLint
+import android.content.res.Configuration
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
+import com.afdal.webapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        webView = binding.webView
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        webView.loadUrl("https://www.islambook.com/")
+        //   binding.webView.loadUrl("https://www.youtube.com/")
+
+        // this will enable the javascript settings
+        webView.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        webView.settings.setSupportZoom(true)
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            Log.d(TAG, "SUPPORTED")
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES -> {
+                    WebSettingsCompat.setForceDark(
+                        webView.settings,
+                        WebSettingsCompat.FORCE_DARK_ON
+                    )
+                }
+                Configuration.COLOR_MODE_HDR_NO, Configuration.UI_MODE_NIGHT_UNDEFINED -> {
+                    WebSettingsCompat.setForceDark(
+                        webView.settings,
+                        WebSettingsCompat.FORCE_DARK_OFF
+                    )
+                }
+            }
+        }
+        Log.d(TAG, "NOT SUPPORTED")
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (webView.canGoBack()) webView.goBack()
+        else super.onBackPressed()
+    }
+}
